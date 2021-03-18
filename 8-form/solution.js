e875092de229dd405f9653ef1783a9d507d9afb1
Eduardo Melo <eduardoccmelo@gmail.com>
2021-03-18 12:28:14 +0100

    number 8 done

--- 8-form/solution.js
+++ 8-form/solution.js
@@ -1,1 +1,20 @@
 const data = [];
+
+let form = document.querySelector("form");
+let button = document.querySelector("button");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let person = {
+    name: form["name"].value,
+    age: form["age"].value,
+    married: form["married"].checked,
+    hobbies: form["hobbies"].value,
+  };
+  console.log(form["name"].value);
+  console.log(form["age"].value);
+  console.log(form["married"].checked);
+  console.log(form["hobbies"].value);
+  form.reset();
+  data.push(person);
+});
